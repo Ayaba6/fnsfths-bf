48d9718406c12097da3d07a913f88a99605ae052
Ayaba6 <ramodgwende421@gmail.com>
2026-06-29 16:35:04 +0000

    initialisation du pwa

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,39 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      // Incluez ici vos assets fixes qui ne sont pas dans le manifeste
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Fédération Nationale Sans Frontière des Tradipraticiens et Herboristes de Santé du Burkina Faso',
+        short_name: 'FNSFTHS',
+        description: 'Plateforme de gestion des membres et réseaux de la FNSFTHS-BF',
+        theme_color: '#16a34a',
+        background_color: '#ffffff',
+        display: 'standalone',
+        icons: [
+          {
+            src: '/logo192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any maskable'
+          },
+          {
+            src: '/logo512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ],
   build: {
-    // Augmente la limite d'avertissement à 1500kb pour les grosses libs (ex: react-qr-code, react-to-print)
     chunkSizeWarningLimit: 1500,
   }
 })
